bbc4c9928ba606254598dcc49f94cd545f0789ad
David Gabrielian <davidgabrielofficial@gamil.com>
2020-07-21 19:07:04 +0400

    Updates

---
+++ precache-manifest.d339454546f1e59ba3526194002a10ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0188629eaa54f12cfb2849e1ffc79d",
+    "url": "/React-Coin/index.html"
+  },
+  {
+    "revision": "6b37ebb4c899e342c99c",
+    "url": "/React-Coin/static/css/main.7e877c81.chunk.css"
+  },
+  {
+    "revision": "da8bfaf9f9fdfa64feb5",
+    "url": "/React-Coin/static/js/2.11995e7d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Coin/static/js/2.11995e7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b37ebb4c899e342c99c",
+    "url": "/React-Coin/static/js/main.cd30fed6.chunk.js"
+  },
+  {
+    "revision": "3a8042623b91f3c38065",
+    "url": "/React-Coin/static/js/runtime-main.242095ec.js"
+  },
+  {
+    "revision": "5c0436d55f12af99760ca65640d19d19",
+    "url": "/React-Coin/static/media/logo.5c0436d5.png"
+  }
+]);
